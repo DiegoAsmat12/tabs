b519800669a33736410450860b70f9bd9a9cb339
DiegoAsmat12 <diegoas1205@gmail.com>
2022-03-22 12:43:50 -0500

    Ejercicio Finalizado

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import Tabs from './Componentes/Tabs';
+
 
 function App() {
+  const [tabs,setTabs] = useState([{
+    etiqueta:"Tab 1",
+    contenido:"Contenido de Tab 1",
+    selected:true
+  },{
+    etiqueta:"Tab 2",
+    contenido:"Contenido de Tab 2",
+    selected:false
+  }]);
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Tabs tabs={tabs} setTabs={setTabs}/>
     </div>
   );
 }
